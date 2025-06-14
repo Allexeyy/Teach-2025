a53c589e8f3f4ba9e28bc61a144e179161659604
Allexeyy <alexey.pasunkin@gmail.com>
2025-06-14 12:59:03 +0300

    edit script elk

---
+++ elk_i.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#Устновка java и копирование его конфига
+sudo apt update
+sudo apt install default-jdk -y
+cp /home/alexey/repo/jvm.options /etc/elasticsearch/jvm.options.d/jvm.options
+
+#Устновка ElasticSearch и копирование его конфига
+sudo dpkg -i elasticsearch-8.9.1-amd64.deb
+cp /home/alexey/repo/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
+sudo systemctl daemon-reload
+sudo systemctl enable --now elasticsearch.service
+
+#Устновка Kibana и копирование его конфига
+sudo dpkg -i kibana-8.9.1-amd64.deb
+cp /home/alexey/repo/kibana.yml /etc/kibana/kibana.yml
+sudo systemctl daemon-reload
+sudo systemctl enable --now kibana.service
+sudo systemctl status kibana.service
+
+#Устновка Logstash и копирование его конфига
+sudo dpkg -i logstash-8.9.1-amd64.deb
+cp /home/alexey/repo/logstash/logstash.yml /etc/logstash/logstash.yml
+cp /home/alexey/repo/logstash-nginx-es.conf /etc/logstash/conf.d/logstash-nginx-es.conf
+sudo systemctl enable --now logstash.service
+sudo systemctl restart logstash.service
+
+#Устновка FileBeat и копирование его конфига
+#Примечание при установке на текущий сервер требуется установить WEB-сурвер на текущий сервер
+#dpkg -i filebeat-8.9.1-amd64.deb
+#cp /home/alexey/repo/filebeat.yml /etc/filebeat/filebeat.yml
+#systemctl enable filebeat
+#systemctl restart filebeat
+
