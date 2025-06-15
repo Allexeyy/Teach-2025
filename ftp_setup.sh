731696c93008a8386a7c44721b6a06d4495cce86
Allexeyy <84806636+Allexeyy@users.noreply.github.com>
2025-06-15 14:02:24 +0300

    Update ftp_setup.sh

--- ftp_setup.sh
+++ ftp_setup.sh
@@ -21,27 +21,10 @@ check_success "установка vsftpd"
 # Резервное копирование оригинального конфигурационного файла
 echo "Создание резервной копии конфигурационного файла vsftpd..."
 sudo cp /etc/vsftpd.conf /etc/vsftpd.conf.bak
-check_success "резервное копирование конфигурации"
 
 # Настройка конфигурации vsftpd
 echo "Настройка конфигурации vsftpd..."
-
-# Включаем доступ для локальных пользователей
-sudo bash -c "echo 'local_enable=YES' >> /etc/vsftpd.conf"  # Разрешает локальным пользователям вход в FTP
-
-# Разрешаем запись для локальных пользователей
-sudo bash -c "echo 'write_enable=YES' >> /etc/vsftpd.conf"  # Позволяет локальным пользователям загружать файлы
-
-# Включаем chroot для локальных пользователей
-sudo bash -c "echo 'chroot_local_user=YES' >> /etc/vsftpd.conf"  # Ограничивает пользователей их домашними директориями
-
-# Стилизация директории при входе
-sudo bash -c "echo 'allow_writeable_chroot=YES' >> /etc/vsftpd.conf"  # Позволяет запись в chroot-окружении.
-
-# Разрешаем пассивный режим
-sudo bash -c "echo 'pasv_enable=YES' >> /etc/vsftpd.conf"  # Включает пассивный режим FTP
-sudo bash -c "echo 'pasv_min_port=10000' >> /etc/vsftpd.conf"  # Задает минимальный порт для пассивного режима
-sudo bash -c "echo 'pasv_max_port=10100' >> /etc/vsftpd.conf"  # Задает максимальный порт для пассивного режима
+sudo cp /home/alexey/repo/vsftpd.conf /etc/vsftpd.conf
 
 # Перезапуск службы vsftpd для применения настроек
 echo "Перезапуск службы vsftpd..."
