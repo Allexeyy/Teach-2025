48b76f8620025dea3ae62b7c1fd1158301ae67ca
Allexeyy <84806636+Allexeyy@users.noreply.github.com>
2025-06-17 21:48:29 +0300

    Update elk_i.sh

--- elk_i.sh
+++ elk_i.sh
@@ -6,23 +6,25 @@ sudo apt install default-jdk -y
 
 #Устновка ElasticSearch и копирование его конфига
 sudo dpkg -i elasticsearch-8.9.1-amd64.deb
-sudo cp /home/alexey/repo/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
-sudo cp /home/alexey/repo/jvm.options /etc/elasticsearch/jvm.options.d/jvm.options
 sudo systemctl daemon-reload
 sudo systemctl enable --now elasticsearch.service
+sudo cp /home/alexey/repo/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
+sudo cp /home/alexey/repo/jvm.options /etc/elasticsearch/jvm.options.d/jvm.options
+sudo systemctl restart elasticsearch.service
 
 #Устновка Kibana и копирование его конфига
 sudo dpkg -i kibana-8.9.1-amd64.deb
-sudo cp /home/alexey/repo/kibana.yml /etc/kibana/kibana.yml
 sudo systemctl daemon-reload
 sudo systemctl enable --now kibana.service
-sudo systemctl status kibana.service
+sudo cp /home/alexey/repo/kibana.yml /etc/kibana/kibana.yml
+sudo systemctl restart kibana.service
 
 #Устновка Logstash и копирование его конфига
 sudo dpkg -i logstash-8.9.1-amd64.deb
+sudo systemctl daemon-reload
+sudo systemctl enable --now logstash.service
 sudo cp /home/alexey/repo/logstash.yml /etc/logstash/logstash.yml
 sudo cp /home/alexey/repo/logstash-nginx-es.conf /etc/logstash/conf.d/logstash-nginx-es.conf
-sudo systemctl enable --now logstash.service
 sudo systemctl restart logstash.service
 
 #Устновка FileBeat и копирование его конфига
@@ -31,4 +33,3 @@ sudo systemctl restart logstash.service
 #cp /home/alexey/repo/filebeat.yml /etc/filebeat/filebeat.yml
 #systemctl enable filebeat
 #systemctl restart filebeat
-
